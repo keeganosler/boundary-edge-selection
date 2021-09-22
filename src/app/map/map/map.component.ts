997f20f1ea091aef3fcca3e2a8e46537cab41d4d
Keegan Osler <keegan.osler@vergeag.com>
2021-09-22 15:08:00 -0600

    basic full page OL map

--- src/app/map/map/map.component.ts
+++ src/app/map/map/map.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { Map, View } from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import { fromLonLat } from 'ol/proj';
+import XYZ from 'ol/source/XYZ';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.scss'],
 })
-export class MapComponent implements OnInit {
-
-  constructor() { }
+export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
+  constructor() {}
+  @ViewChild('viewMap', { static: true }) private viewMap: ElementRef;
+  map: Map;
+  layers: TileLayer<XYZ>[] = [];
+  satellite = new TileLayer({
+    source: new XYZ({
+      url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    }),
+  });
 
   ngOnInit(): void {
+    setTimeout(() => this.map?.updateSize());
   }
 
+  ngOnDestroy(): void {}
+
+  ngAfterViewInit(): void {
+    this.layers.push(this.satellite);
+    this.map = new Map({
+      controls: [],
+      target: this.viewMap.nativeElement,
+      layers: this.layers,
+      overlays: [],
+      view: new View({
+        center: fromLonLat([-101.320426, 42.041532]),
+        zoom: 4,
+      }),
+    });
+  }
 }
